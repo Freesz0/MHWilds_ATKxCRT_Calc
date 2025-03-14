865e2d6acafafb0ed064976aeee0e9d97db7f826
Freesz0 <matheusfreesz1@gmail.com>
2025-03-14 19:04:32 -0300

    Added final dmg calculation and comparator function, tidied up comments

--- calc.py
+++ calc.py
@@ -1,8 +1,5 @@
 import math
 
-import calc
-
-
 def apply_attack_boost(raw, bonus_raw, ab_level):
     # raw: raw attack value
     # bonus_raw: bonus raw attack value (food, powercharm, etc.)
@@ -25,11 +22,9 @@ def apply_attack_boost(raw, bonus_raw, ab_level):
 
 def apply_critical_boost(crit_boost_level):
     # crit_boost_level: critical boost level
-    # min keeps value below 1.4
-    # max keeps value above 1.25
     crit_mod = 1.25 + (crit_boost_level * 0.03)
-    crit_mod = min(crit_mod, 1.4)
-    crit_mod = max(crit_mod, 1.25)
+    crit_mod = min(crit_mod, 1.4) # Keeps value below 1.4 (max CB buff)
+    crit_mod = max(crit_mod, 1.25) # Keeps value above 1.25 (min crit modifier)
     return crit_mod
 
 def calc_effective_crit_mod(crit_mod, affinity):
@@ -47,12 +42,20 @@ def calc_total_dmg(raw, bonus_raw, affinity, ab_level, cb_level):
     # MH adds 0.1 to any damage value and then rounds it down, pushing 0.9 values up
     total_raw = apply_attack_boost(raw, bonus_raw, ab_level)
     crit_mod = apply_critical_boost(cb_level)
-    effective_crit_mod = calc_effective_crit_mod(crit_mod, affinity)
+    effective_crit_mod = calc_effective_crit_mod(crit_mod, affinity) # Returns the effective modifier taking affinity in consideration
     total_dmg = (total_raw * effective_crit_mod) + 0.1
-    return math.floor(total_dmg)
+    return math.floor(total_dmg) # Needed to round down after 0.1 round up
+
+def dmg_comparator(raw, bonus_raw, affinity, ab_level_a, cb_level_a, ab_level_b, cb_level_b):
+    damage_a = calc_total_dmg(raw, bonus_raw, affinity, ab_level_a, cb_level_a)
+    damage_b = calc_total_dmg(raw, bonus_raw, affinity, ab_level_b, cb_level_b)
+    damage_diff = round((damage_a - damage_b) / damage_a, 2) # Provides difference between damages in a percentile, rounded up with 2 decimals
+    comparator = damage_a, damage_b, damage_diff
+    return comparator
 
 if __name__ == "__main__":
     print(f'Total Raw Damage: {apply_attack_boost(100, 5, 5)}')
     print(f'Critical Modifier: {apply_critical_boost(5)}')
     print(f'Effective crit modifier: {calc_effective_crit_mod(1.25, 0)}')
-    print(f'Total dmg: {calc_total_dmg(100, 3, 100, 5, 0)}')
+    print(f'Total dmg: {calc_total_dmg(100, 3, 20, 5, 3)}')
+    print(f'Comparison: {dmg_comparator(100, 3, 20, 5, 3, 3, 5)}')
